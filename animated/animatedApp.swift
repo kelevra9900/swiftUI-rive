4a690ff4f5acf6903d50df2724ce5d0eac4fcde5
Roger Torres <torresroger445@gmail.com>
2022-11-30 16:28:47 -0700

    Initial Commit

---
+++ animated/animatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  animatedApp.swift
+//  animated
+//
+//  Created by Rogelio on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct animatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
